70e0884e7e5bcdb25071934892b6b85c2dd47fed
Kevin Arce <arcegk@gmail.com>
2016-07-06 02:30:09 -0500

    try

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
+console.log("entrando al web.js");
 var gzippo = require('gzippo');
 var express = require('express');
+console.log("entrando a morgan");
 var morgan = require('morgan');
 var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000, function(){
-	console.log("servidor iniciado");
-});
+app.listen(process.env.PORT || 5000);
